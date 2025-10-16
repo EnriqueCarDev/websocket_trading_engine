d546d17db0ae14cc09ddd7c70735dd95f5b00467
Enrique <73123851+F4l4X@users.noreply.github.com>
2025-10-16 23:06:30 +0200

    modified Order class

--- include/Order.hpp
+++ include/Order.hpp
@@ -1,37 +1,44 @@
 #pragma once
 
-#include "./enums.hpp"
 #include "./types.hpp"
 #include <vector>
 
 class Order {
-  trading::Id id_;
-  trading::Price price_;
-  trading::Side side_;
-  trading::OrderType type_;
-  trading::Quantity initial_quantity_;
-  trading::Quantity remaining_quantity_;
+  enum Side { BUY, SELL };
+  enum OrderType { FillAndKill, GoodTillCancel, GoodForDay };
+  std::string id_;
+  std::uint32_t price_;
+  std::uint32_t avgPrice_ = 0;
+  Side side_;
+  OrderType type_;
+  std::uint32_t initial_quantity_;
+  std::uint32_t remaining_quantity_;
+  std::uint32_t cumQty_ = 0;
 
 public:
-  Order(trading::Id id, trading::Price price, trading::Side side,
-        trading::OrderType type, trading::Quantity initial_quantity)
+  Order(std::string id, std::uint32_t price, Side side, OrderType type,
+        std::uint32_t initial_quantity)
       : id_{id}, price_{price}, side_{side}, type_{type},
         initial_quantity_{initial_quantity},
         remaining_quantity_{initial_quantity} {}
 
-  trading::Id getId() const { return id_; }
-  trading::Price getPrice() const { return price_; }
-  trading::Side getSide() const { return side_; }
-  trading::OrderType getType() const { return type_; }
-  trading::Quantity getInitialQuantity() const { return initial_quantity_; }
-  trading::Quantity getRemainingQuantity() const { return remaining_quantity_; }
+  const std::string &getId() const { return id_; }
+  const std::uint32_t &getPrice() const { return price_; }
+  const Side &getSide() const { return side_; }
+  const OrderType &getType() const { return type_; }
+  const std::uint32_t &getInitialQuantity() const { return initial_quantity_; }
+  const std::uint32_t &getRemainingQuantity() const {
+    return remaining_quantity_;
+  }
 
   bool isFilled() const { return remaining_quantity_ == 0; }
 
-  void fillOrder(trading::Quantity quantity) {
+  void fillOrder(std::uint32_t quantity) {
     if (quantity > remaining_quantity_)
       throw std::logic_error("Quantity is superior to remaining quantity");
 
     remaining_quantity_ -= quantity;
+    avgPrice_ =
+        (avgPrice_ * cumQty_) + (price_ * quantity_) / (cumQty + quantity);
   }
 };
